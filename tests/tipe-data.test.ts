d513d73575e5ebaf4091fb3ec6823f84b00e375f
MuhammadRavi <muhammadravi5@gmail.com>
2024-10-27 01:16:08 +0700

    Tipe data primitif & add dist at gitignore

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,7 @@
+describe("Tipe Data", function () {
+    it('Should Must Desclare', function () {
+        const name: string = "Muhammad Ravi";
+        const balance: number = 10000;
+        const isVip: boolean = true;
+    })
+})
